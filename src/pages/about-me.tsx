17f81d19a2f740ee01531a235cd695e19889a9f5
LewisJones2021 <81636428+LewisJones2021@users.noreply.github.com>
2023-03-18 16:20:44 +0000

    styles: updated styles for small screens

--- src/pages/about-me.tsx
+++ src/pages/about-me.tsx
@@ -8,16 +8,16 @@ export const aboutMe = () => {
   <div className="mx-auto max-w[60%] h-screen mt-[60px]">
    <h1 className=" text-gray-400 flex justify-center text-2xl mx-auto font-header">Hey 👋</h1>
    <div className="flex justify-center flex-col max-w-xl p-5 mx-auto font-cardContent ">
-    <p className="text-gray-500 md:text-xl leading-3 ">
+    <p className="text-gray-500 md:text-xl md:leading-6 ">
      I’m an aspiring software developer using multiple technologies, for example: React, JavaScript, HTML5 and CSS. I’ve
      been learning software development in a self taught environment for a couple of years, whilst working on open
      source web applications and using online video courses and creating personal mini projects.
     </p>
-    <p className="text-gray-500 md:text-xl mt-5 leading-3">
+    <p className="text-gray-500 md:text-xl mt-5 md:leading-6">
      Alongside my current tech stack of React, Svelte, HTML, CSS, Tailwind, JavaScript, NPM and Git, I aim to become a
      full-stack developer using different technoliges such as AWS, TypeScript, Python and Golang.
     </p>
-    <p className="text-gray-500 md:text-xl mt-5 leading-3">
+    <p className="text-gray-500 md:text-xl mt-5 md:leading-6">
      My main focus recently has been contributing to an open source time managment web application. I am currently
      seeking my first commmercial developer role.
     </p>
