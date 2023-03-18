f83540978fafc16ba5b0e5b088e8863223de72e2
LewisJones2021 <81636428+LewisJones2021@users.noreply.github.com>
2023-03-18 17:12:09 +0000

    styles:fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,7 +44,7 @@ export const Home = () => {
         <img src={card.imageSrc} className="max-w-[180px] rounded-lg shadow-2xl md:max-w-[300px] " />
         <div>
          <h1 className="text-sm font-bold md:text-xl">{card.title}</h1>
-         <p className="py-7 font-cardContent max-w-[250px] text-sm md:text-lg md:max-w-xl ">{card.description}</p>
+         <p className="py-6 font-cardContent max-w-[250px] text-sm md:text-lg md:max-w-xl ">{card.description}</p>
          <button className="btn  bg-green-900 font-button">
           <Link href={card.link}>View project</Link>
          </button>
