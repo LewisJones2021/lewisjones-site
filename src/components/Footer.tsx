a764de14792476fccc35f9f03078fa2084f5058a
LewisJones2021 <81636428+LewisJones2021@users.noreply.github.com>
2023-03-18 18:12:21 +0000

    footer fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Link from 'next/link';
 export const Footer = () => {
  return (
-  <div className=" flex justify-between text-sm text-gray-400 pt-8 p-[25px] w-full  bg-green-900 font-header md:text-lg">
+  <div className=" flex justify-between text-[8px] text-gray-400 pt-8 p-[25px] w-full  bg-green-900 font-header md:text-sm">
    <p className="   ">Made by Lewis, featuring lots of ☕️</p>
    <p className=" ">
     Find me on&nbsp;
