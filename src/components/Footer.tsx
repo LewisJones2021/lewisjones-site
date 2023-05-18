39d6e02493dad4cbca0339259156a4166c394804
LewisJones2021 <81636428+LewisJones2021@users.noreply.github.com>
2023-05-18 16:42:14 +0100

    styles: content overlaping issue fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Link from 'next/link';
 export const Footer = () => {
  return (
-  <div className="flex flex-col text-[10px] text-gray-400 h-[200px] px-[20px] py-[20px] w-full bg-gray-800 font-header absoloute mt-0 md:text-sm">
+  <div className="flex flex-col text-[10px] text-gray-400 h-[200px] px-[20px] py-[20px] w-full bg-gray-800 font-header mt-0 md:text-sm">
    <p className="mb-5">Made by Lewis, featuring lots of ☕️</p>
    <p className="flex">
     Find me on &nbsp;
